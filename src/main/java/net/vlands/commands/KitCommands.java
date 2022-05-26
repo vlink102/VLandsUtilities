806673bd3cfff57e0b07d19d67ebddad7eba5b04
ETAN <emstan@justalf.com>
2022-05-26 22:45:21 +0100

    Like no changes because i was revising,

--- src/main/java/net/vlands/commands/KitCommands.java
+++ src/main/java/net/vlands/commands/KitCommands.java
@@ -4,27 +4,34 @@ import net.vlands.VLandsUtilities;
 import net.vlands.kits.Kit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
 import revxrsal.commands.annotation.Command;
 import revxrsal.commands.annotation.Dependency;
 import revxrsal.commands.annotation.Named;
+import revxrsal.commands.annotation.Subcommand;
 import revxrsal.commands.bukkit.annotation.CommandPermission;
 import revxrsal.commands.command.ArgumentStack;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class KitCommands {
 
     @Dependency
     private VLandsUtilities plugin;
 
-    @Command("createkit")
+    @Command("kit")
+    @Subcommand("create") // TODO idk how to use subcommands in lamp there isnt many examples.
     @CommandPermission("vlands.commands.createkit")
     public void createKit(Player player,@Named("internal name") String internalName, @Named("cost") Double cost, @Named("formatted name") ArgumentStack stack) {
         String formattedName = ChatColor.translateAlternateColorCodes('&', stack.join(" "));
         if (formattedName.equals("") || internalName.equals("")) return;
 
-        Kit kit = new Kit(internalName, formattedName, player.getEquipment().getArmorContents(), player.getInventory().getContents(), cost, 60000); // TODO idk how potioneffects work here, maybe only alllow from yaml.
+        List<PotionEffect> effectList = new ArrayList<>(player.getActivePotionEffects());
+        effectList.forEach(potionEffect -> potionEffect = new PotionEffect(potionEffect.getType(), 1200 /* 1 minute */, potionEffect.getAmplifier()));
+
+        Kit kit = new Kit(internalName, formattedName, player.getEquipment().getArmorContents(), player.getInventory().getContents(), effectList, cost, 60000); // TODO idk how potioneffects work here, maybe only alllow from yaml.
         this.plugin.getKitManager().addOrReplaceKit(kit);
     }
-
-
 
 }
--- src/main/java/net/vlands/kits/Kit.java
+++ src/main/java/net/vlands/kits/Kit.java
@@ -1,7 +1,6 @@
 package net.vlands.kits;
 
 import lombok.Getter;
-import lombok.Setter;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
@@ -10,9 +9,6 @@ import org.bukkit.potion.PotionEffectType;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.IntFunction;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 public class Kit {
 
@@ -22,22 +18,22 @@ public class Kit {
     private final String formattedName;
     private final ItemStack[] armorItems;
     private final ItemStack[] inventoryItems;
-    private List<PotionEffect> potionEffects;
+    private final List<PotionEffect> potionEffects;
     @Getter
     private final double cost;
     @Getter
-    private long cooldown;
+    private final long cooldown;
 
     /**
-     * @param internalName   The simple string that will be used in the kit command - /kit (commandIdentifier).
-     * @param formattedName  The color coded, formatted string that will be used to display the kit's name in messages and GUIs.
-     * @param armorItems     The 4 armor items - Helmet, Chestplate, Leggings, Boots.
-     * @param potionEffects  The potion effects to add to the player whilst using the kit TODO add this functionality
-     * @param cooldown       The cooldown of the kit
-     * @param inventoryItems The list of items to be added to the inventory.
-     * @param cost           The amount to withdraw from the player's balance on receiving the kit.
+     * @param internalName    The simple string that will be used in the kit command - /kit (commandIdentifier).
+     * @param formattedName   The color coded, formatted string that will be used to display the kit's name in messages and GUIs.
+     * @param armorItems      The 4 armor items - Helmet, Chestplate, Leggings, Boots.
+     * @param potionEffects   The potion effects to add to the player
+     * @param cooldown        The cooldown of the kit
+     * @param inventoryItems  The list of items to be added to the inventory.
+     * @param cost            The amount to withdraw from the player's balance on receiving the kit.
      */
-    public Kit(String internalName, String formattedName, ItemStack[] armorItems, ItemStack[] inventoryItems,List<PotionEffect> potionEffects, double cost, long cooldown) {
+    public Kit(String internalName, String formattedName, ItemStack[] armorItems, ItemStack[] inventoryItems, List<PotionEffect> potionEffects, double cost, long cooldown) {
         this.internalName = internalName.toLowerCase();
         this.formattedName = formattedName;
 
@@ -53,6 +49,7 @@ public class Kit {
             if (inventoryItems[i] != null && inventoryItems[i].getType() == Material.AIR)
                 inventoryItems[i] = null;
         }
+
         this.inventoryItems = Arrays.copyOf(inventoryItems, 36);
         this.cost = cost;
         this.cooldown = cooldown;
